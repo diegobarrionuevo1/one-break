3eb7db49e7817e29115cb85c6108e8b3cf146826
diegobarrionuevo1 <diegobarrionuevo11@gmail.com>
2025-06-04 22:42:27 -0300

    feat: agrega librería lottie-react y componentes de logo animado

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,34 @@
 'use client'
 import { Button } from "@/components/ui/button";
 import { IconWhatsapp } from "@/public/icons";
-import Image from "next/image";
+import LogoLottie from "@/public/Logo_lootie";
 import Link from 'next/link';
-import { useState } from "react";
 
 
 export default function Home() {
-  const [isVideoReady, setIsVideoReady] = useState(false);
+
   const whatsappLink = "https://api.whatsapp.com/send/?phone=+5493513853153&text=Hola!+Mi+amigo+Diego+Barrionuevo+es+un+crack!";
   return (
     <div className=" font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center justify-center w-full ">
         <div className="relative  w-full h-screen overflow-hidden ">
-          {/* Imagen de placeholder hasta que el video esté listo */}
-          {!isVideoReady && (
-            <Image
-              src="/primer-cuadro.jpg"
-              alt="Primer cuadro del video"
-              fill
-              className="object-cover absolute top-0 left-0 z-10"
-            />
-          )}
+
           <video
-            src="https://au3p4exjgd78giaz.public.blob.vercel-storage.com/comprimido-l5ffA84PxqWSqnm4InBcFDRujBxs3C.mp4"
+            src="https://pub-529e4be87aae4f07a6e3cdfd1f8e2a00.r2.dev/0604.mp4"
             autoPlay
             loop
             muted
-            className={`w-full h-full object-cover transition-opacity duration-300 ${isVideoReady ? "opacity-100" : "opacity-0"}`}
-            onCanPlay={() => setIsVideoReady(true)}
+            className={`w-full h-full object-cover transition-opacity duration-300`}
             playsInline
+            poster="/primer-cuadro.jpg"
           />
           <div className="absolute flex flex-col gap-5 items-center justify-center z-50  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ">
-            <div className=" overflow-hidden h-[300px] min-w-[550px] xl:flex xl:items-center xl:justify-center  ">
-              <video src="https://au3p4exjgd78giaz.public.blob.vercel-storage.com/Logo-3-vS3LvZwZtD4sdU1D4g0Ux4eqHT3Vr3.webm" autoPlay muted className="w-full  " />
+            <div className=" h-[300px] min-w-[550px] xl:flex xl:items-center xl:justify-center  ">
+              <LogoLottie />
             </div>
             <Button
               variant="outline"
-              className="flex text-xl w-[200px] h-[40px] text-white font-bold border-2 border-white  bg-transparent hover:text-white hover:bg-transparent hover:scale-105 transition-all"
+              className="flex z-10 text-xl w-[200px] h-[40px] text-white font-bold border-2 border-white  bg-transparent hover:text-white hover:bg-transparent hover:scale-105 transition-all"
               asChild
             >
               <Link
